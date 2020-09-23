b3838f296184dbe717d510e7300909f18a3b4342
orlandoCarpio <orlando_carpio85@hotmail.com>
2020-09-23 03:40:59 -0300

    archivo funciones.php completo

--- prueba php/Funciones.php
+++ prueba php/Funciones.php
@@ -4,7 +4,18 @@
         public function __construct($id){
             $this->id=$id;
         }
-        
+        public function entero(){
+            return (preg_match("/^([0-9])*$/",$this->id))?true:false;
+        }
+        public function mayor(){
+            return ($this->id>=2)?true:false;
+        }
+        public function par(){
+            return ($this->id%2==0)?true:false;
+        }
+        public function positivo(){
+            return (is_int(+$this->id))?true:false;
+        }
         public function guardar(){
             $_SESSION['array'][]=$this->id;
             return;
